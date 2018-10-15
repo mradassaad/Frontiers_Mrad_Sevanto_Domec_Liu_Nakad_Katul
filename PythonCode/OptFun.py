b7b5f7e866a013ceadca2806a20abe2a2394432b
Assaad Mrad <mradassaad2@gmail.com>
2018-10-15 19:20:57 -0400

    update 10/15

--- PythonCode/OptFun.py
+++ PythonCode/OptFun.py
@@ -27,6 +27,77 @@ unit3 = 1e6  # Pa -> MPa
 unit4 = 273.15  # Degree C -> K
 
 
+def A(a1, a2, ca, cp, g):
+    """
+
+    :param a1: Parameter corresponding to either light- or Rubisco-limited photosynthesis
+    :param a2: Parameter corresponding to either light- or Rubisco-limited photosynthesis
+    :param ca: ambient CO2 mole fraction in the air in mmol/mol
+    :param cp: CO2 concentration at which assimilation is zero or compensation point in mmol/mol
+    :param g: stomatal conductance in umol/m2/s
+    :return: value of A at a particular value of g
+    """
+    gamma = np.sqrt((a2 + ca + a1/g) ** 2 + 4 * a1 * (cp - ca) / g)
+
+    return 0.5 * (a1 + g * (ca + a2) + g * gamma)
+
+
+def dAdg(a1, a2, ca, cp, g):
+    """
+
+    :param a1: Parameter corresponding to either light- or Rubisco-limited photosynthesis
+    :param a2: Parameter corresponding to either light- or Rubisco-limited photosynthesis
+    :param ca: ambient CO2 mole fraction in the air in mmol/mol
+    :param cp: CO2 concentration at which assimilation is zero or compensation point in mmol/mol
+    :param g: stomatal conductance in umol/m2/s
+    :return: value of the partial derivative of A with respect to g at a particular value of g
+    """
+    ca *= 1e3
+    cp *= 1e3
+    gamma = np.sqrt((a2 + ca + a1 / g) ** 2 + 4 * a1 * (cp - ca) / g)
+
+    return (1 / (2 * g * gamma)) * (g * (a2 + ca) ** 2 + a1 * (a2 - ca) + (a2 + ca) * g * gamma)
+
+
+def a_light(alpha_p, em, par, cp):
+    """
+
+    :param alpha_p: absorptivity of the leaf for PAR
+    :param em: maximum quantum efficiency in mol/mol
+    :param par: PAR photon flux density incident on the leaf in umol/m2/s
+    :param cp: CO2 concentration at which assimilation is zero or compensation point in mmol/mol
+    :return: a1 in umol/m2/s and a2 in umol/mol
+    """
+    a1 = alpha_p * em * par
+    a2 = 2 * cp * 1e3
+    return a1, a2
+
+
+def a_rubisco(vm, kc, coa, ko):
+    """
+
+    :param vm: the rubisco capacity in umol/m2/s
+    :param kc: Michaelis constant for CO2 in umol/mol
+    :param coa: oxygen mole fraction in the air in mmol/mol
+    :param ko: inhibition constant for O2 in mmol/mol
+    :return: a1 in umol/m2/s and a2 in umol/mol
+    """
+    a1 = vm
+    a2 = kc * (1 + coa / ko)
+    return a1, a2
+
+def vm_calc(vm_25, Tl):
+    """
+
+    :param vm_25:  value of the rubisco capacity at leaf temperature Tl of 25C in umol/m2/s
+    :param Tl: leaf temperature in C
+    :return: the rubisco capacity Vm in umol/m2/s
+    """
+    # The optimal temperature is considered 25C
+    # The cutoff temperature is assumed 41C
+    # For reference, check Norman and Campbell page 239-241
+    return vm_25 * np.exp(0.088 * (Tl - 25)) / (1 + np.exp(0.29 * (Tl - 41)))
+
 def dydt(t, y):
     """
     y[0] is lambda(t) and y[1] is x(t)
